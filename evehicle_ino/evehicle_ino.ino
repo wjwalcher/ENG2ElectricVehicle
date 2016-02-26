de5c52a0733b83ae4132eafe36f876fd42f7652e
unknown <Student@STU-ENG14.Provnet.local>
2016-02-26 10:55:47 -0600

    Adds in reverse

---
+++ evehicle_ino/evehicle_ino.ino
@@ -0,0 +1,60 @@
+//The RUFF RIDERS Electric Vehicle Team
+
+//Imports 
+#include <Servo.h>
+
+//Set port numbers for I/O 
+int pedalPot = 0;
+int victorPort = 3;
+int btnReverse = 2;
+int btnNeutral = 3;
+int btnDrive = 4;
+int ledReverse = 9;
+int ledNeutral = 10;
+int ledDrive = 11;
+
+int minPotRange = 0;
+int maxPotRange = 200;
+
+//Start the vehicle in neutral (for safety reasons)
+boolean drive = false;
+boolean neutral = true;
+boolean reverse = false;
+
+Servo victor;
+
+void setup(){
+  victor.attach(victorPort);
+}
+
+void loop(){
+  pedalVal = pedalPot.analogRead(pedalPot);
+  pedalVal = map(pedalVal, 0, 1023, minPotRange, maxPotRange);
+  if(btnDrive == HIGH){
+    drive = true;
+    neutral = false;
+    reverse = false;
+    if(drive && pedalVal >= 10){
+      writeVal = pedalVal * 0.5 + 90; //Fix this. Currently, this will cause the car to run in reverse... yikes!
+      if(writeVal <= 180){ //Failsafe. Prevents program from crashing if value is greater than 180
+        victor.write(writeVal);
+      }
+    }  
+   if(btnNeutral == HIGH){
+     neutral = true;
+     drive = false;
+     reverse = false;
+     if(neutral){
+      victor.write(90); 
+      }
+    }
+    if(btnReverse == HIGH){
+      reverse = true;
+      drive = false;
+      neutral = false;
+      if(reverse && pedalVal >= 10){
+       writeVal = pedalVal; 
+       victor.write(writeVal);;
+      }  
+    }
+}
